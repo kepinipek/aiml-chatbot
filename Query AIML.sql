273ba0e477c3f47145bbf605838df381337a846b
kepinipek <117752003+kepinipek@users.noreply.github.com>
2023-07-30 22:54:00 +0700

    Add files via upload

---
+++ Query AIML.sql
@@ -0,0 +1,6 @@
+SELECT * FROM conversations;
+SELECT * FROM turns;
+SELECT * FROM missing_questions;
+SELECT * FROM categories;
+SELECT * FROM conversation_properties order by id asc;
+SELECT * FROM users;
